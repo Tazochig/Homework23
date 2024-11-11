998bc3a9e9a3c8eee774bfd109e2dd537185d48a
Tazochig <chigladzetazo@gmail.com>
2024-11-11 10:20:39 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,64 @@
+// gasaketeblebi 2 11 13 16 17 18
+// 1;
+// let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+// let result = numbers.reduce((acc, cur) => {
+//   return acc + cur;
+// }, 0);
+// console.log(result);
+// 2
+// let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+// let result = numbers.reduce(())
+// 4;
+// let sitkvebi = ["gamarjoba", "rogor", "xar"];
+// let gaertianeba = sitkvebi.join(" ");
+// console.log(gaertianeba);
+// 5;
+// let numbers = [1, 2, 3, 4, 5, 6, 7];
+// let result = numbers.reduce((acc, cur) => {
+//   return acc * cur;
+// }, 1);
+// console.log(result);
+// 7;
+// let numbers = [1, 2, 3, 4, 5, 6, 7];
+// let result = numbers.reduce((acc, cur) => {
+//   return acc - cur;
+// });
+// console.log(result);
+// 8;
+// let arr = [1, 2, 3, 4, 5, 6, 7];
+// let changednumbers = arr.map((number) => {
+//   return number * 2;
+// });
+// console.log(changednumbers);
+// 9;
+// let sitkvebi = ["gamarjoba", "rogor", "xar"];
+// let result = sitkvebi.map((name) => name.toUpperCase());
+// console.log(result);
+// 12
+// let arr = [1, 2, 3, 4, 5, 6, 7];
+// let changednumbers = arr.map((number) => {
+//   return number * number;
+// });
+// console.log(changednumbers);
+// 14;
+// const students = [
+//   { name: "Tazo", score: 85 },
+//   { name: "Giorgi", score: 92 },
+//   { name: "Nikoloz", score: 78 },
+//   { name: "Luka", score: 88 },
+//   { name: "Davit", score: 95 },
+// ];
+// students.forEach((student) => {
+//   student.score += 5;
+// });
+// console.log(students);
+// 16;
+// let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+// for (let index = 0; index < array.length; index++) {
+// }
+// 19;
+// let arr = [4, 7, 10, 12, 51];
+// let changedNumbers = arr.filter((num) => {
+//   return num > 10;
+// });
+// console.log(changedNumbers);
